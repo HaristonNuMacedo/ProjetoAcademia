e5061ebe8d52282346ae2426e9aef320eca836a3
HaristonNuMacedo <haristonmacedo2001@gmail.com>
2021-06-24 11:46:02 -0300

    Conectar com o banco de dados

---
+++ conecta.php
@@ -0,0 +1,29 @@
+<?php
+
+    $endereco = 'localhost';
+    $usuario = 'root';
+    $senha = 'senac';
+    $banco = 'academiaAMD';
+
+    $link = mysqli_connect($endereco,$usuario,$senha,$banco)
+    or die('Erro: '.mysqli_error($link));
+
+    $sql = "INSERT INTO cadastroAMD values ";
+    $sql .= "(null,";
+    $sql .= "'".$_POST['nome']."',";
+    $sql .= "'".$_POST['email']."',";
+    $sql .= "'".$_POST['senha']."',";
+    $sql .= "'".$_POST['telefone']."',";
+    $sql .= "'".$_POST['especial']."')";
+
+    echo $sql;
+
+    $consulta = mysqli_query($link,$sql);
+
+    if(!$consulta){
+        die('erro: '.mysqli_error($link));
+    } else {
+        echo 'Matricula efetuada com sucesso!';
+    }
+
+?>
