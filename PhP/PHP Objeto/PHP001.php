14eb84b6b725839ee7347a4644145eab2235c869
HaristonNuMacedo <haristonmacedo2001@gmail.com>
2021-06-28 11:16:17 -0300

    PHP OO

--- PhP/PHP Objeto/PHP001.php
+++ PhP/PHP Objeto/PHP001.php
@@ -11,8 +11,8 @@ $p->vlrVenda = 150.00;
 $p->qtdEstoque = 50;
 
 echo "Dados do Produto: <br>";
-echo "Código; " . number_format($p->id,2) . "<br>";
-echo "Nome; " . $p->nome . "<br>";
-echo "Compra; " . $p->vlrCompra . "<br>";
-echo "Venda; " . $p->vlrVenda . "<br>";
-echo "Estoque; " . $p->qtdEstoque . "<br>";
+echo "Código: " . number_format($p->id,2) . "<br>";
+echo "Nome: " . $p->nome . "<br>";
+echo "Compra: " . $p->vlrCompra . "<br>";
+echo "Venda: " . $p->vlrVenda . "<br>";
+echo "Estoque: " . $p->qtdEstoque . "<br>";
