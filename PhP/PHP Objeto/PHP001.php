1fdf0d56fcaf5b060e55f37c1d7ca0f362a58647
HaristonNuMacedo <haristonmacedo2001@gmail.com>
2021-06-28 12:04:39 -0300

    comit

--- PhP/PHP Objeto/PHP001.php
+++ PhP/PHP Objeto/PHP001.php
@@ -4,15 +4,21 @@ require_once "model/poduto.php";
 
 $p = new produto();
 
-$p->id = 10;
-$p->nome = "Tênis";
-$p->vlrCompra = 100.00;
-$p->vlrVenda = 150.00;
-$p->qtdEstoque = 50;
+$p->setId(12);
+$p->setNome("Processador Intel");
+$p->setVlrCompra(1000.00);
+$p->setVlrVenda(15000.00);
+$p->setQtdEstoque(69);
 
 echo "Dados do Produto: <br>";
-echo "Código: " . number_format($p->id,2) . "<br>";
-echo "Nome: " . $p->nome . "<br>";
-echo "Compra: " . $p->vlrCompra . "<br>";
-echo "Venda: " . $p->vlrVenda . "<br>";
-echo "Estoque: " . $p->qtdEstoque . "<br>";
+echo "Código: " . number_format($p->getId(),2) . "<br>";
+echo "Nome: " . $p->getNome() . "<br>";
+echo "Compra: " . number_format($p->getVlrCompra(),2) . "<br>";
+echo "Venda: " . number_format($p->getVlrVenda(),2) . "<br>";
+echo "Estoque: " . $p->getQtdEstoque() . "<br>";
+
+echo "Pode compra , sou muito fã (y)";
+
+$db = mysqli_connect("localhost:3306","root","senac","test");
+$sql = "insert into produto values ('$id','$nome','$vlrCompra','$vlrVneda','$qtdEstoque')";
+mysqli_query($db, $sql);
--- PhP/PHP Objeto/model/poduto.php
+++ PhP/PHP Objeto/model/poduto.php
@@ -3,10 +3,45 @@
 
 class produto{
 
-    public $id;
-    public $nome;
-    public $vlrCompra;
-    public $vlrVenda;
-    public $qtdEstoque;
-    
+    private $id;
+    private $nome;
+    private $vlrCompra;
+    private $vlrVenda;
+    private $qtdEstoque;
+
+    function setId($id){
+        $this->id = $id;
+    }
+    function getId(){
+        return $this->id;
+    }
+
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+    function getNome(){
+        return $this->nome;
+    }
+
+    function setVlrCompra($vlrCompra){
+        $this->vlrCompra = $vlrCompra;
+    }
+    function getVlrCompra(){
+        return $this->vlrCompra;
+    }
+
+    function setVlrVenda($vlrVenda){
+        $this->vlrVenda = $vlrVenda;
+    }
+    function getVlrVenda(){
+        return $this->vlrVenda;
+    }
+
+    function setQtdEstoque($qtdEstoque){
+        $this->qtdEstoque = $qtdEstoque;
+    }
+    function getQtdEstoque(){
+        return $this->qtdEstoque;
+    }
+
 }
